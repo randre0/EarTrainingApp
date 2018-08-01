0aa729dd1d18674ca5b0f661a3d785b3c3ee4dd5
Rymmy Andre <rymmyandre@ymail.com>
2018-07-31 18:00:45 -0700

    Initial commit

---
+++ app/src/main/java/randre0/com/github/eartraining/ExerciseOneActivity.java
@@ -0,0 +1,13 @@
+package randre0.com.github.eartraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_one);
+    }
+}
